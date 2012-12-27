0cd21446b015e41a5d9aff1363c71094c027c64b
Sergey Khorev <sergey.khorev@gmail.com>
2012-12-27 09:00:01 +0400

    add object handler files

---
+++ QLAddin/ohxl/functions/export.hpp
@@ -0,0 +1,83 @@
+
+/*  
+ Copyright (C) 2004, 2005, 2006 Eric Ehlers
+ 
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+// This file was generated automatically by gensrc.py.  If you edit this file
+// manually then your changes will be lost the next time gensrc runs.
+
+// This source code file was generated from the following stub:
+//      ObjectHandler/gensrc/stubs/stub.excel.exports
+
+
+#ifndef ohxl_export_hpp
+#define ohxl_export_hpp
+
+#pragma comment (linker, "/export:_ohEnumeratedClass")
+#pragma comment (linker, "/export:_ohEnumeratedPair")
+#pragma comment (linker, "/export:_ohEnumeratedType")
+#pragma comment (linker, "/export:_ohListEnumeratedClasses")
+#pragma comment (linker, "/export:_ohListEnumeratedPairs")
+#pragma comment (linker, "/export:_ohListEnumeratedTypes")
+#pragma comment (linker, "/export:_ohRepositoryCollectGarbage")
+#pragma comment (linker, "/export:_ohRepositoryDeleteAllObjects")
+#pragma comment (linker, "/export:_ohRepositoryDeleteObject")
+#pragma comment (linker, "/export:_ohRepositoryListObjectIDs")
+#pragma comment (linker, "/export:_ohRepositoryLogAllObjects")
+#pragma comment (linker, "/export:_ohRepositoryLogObject")
+#pragma comment (linker, "/export:_ohRepositoryObjectCount")
+#pragma comment (linker, "/export:_ohGroup")
+#pragma comment (linker, "/export:_ohGroupList")
+#pragma comment (linker, "/export:_ohGroupSize")
+#pragma comment (linker, "/export:_ohLogFile")
+#pragma comment (linker, "/export:_ohLogLevel")
+#pragma comment (linker, "/export:_ohLogSetFile")
+#pragma comment (linker, "/export:_ohLogSetLevel")
+#pragma comment (linker, "/export:_ohLogWriteMessage")
+#pragma comment (linker, "/export:_ohObjectCallerAddress")
+#pragma comment (linker, "/export:_ohObjectCallerKey")
+#pragma comment (linker, "/export:_ohObjectClassName")
+#pragma comment (linker, "/export:_ohObjectCreationTime")
+#pragma comment (linker, "/export:_ohObjectExists")
+#pragma comment (linker, "/export:_ohObjectIsOrphan")
+#pragma comment (linker, "/export:_ohObjectIsPermanent")
+#pragma comment (linker, "/export:_ohObjectPrecedentIDs")
+#pragma comment (linker, "/export:_ohObjectUpdateCounter")
+#pragma comment (linker, "/export:_ohObjectUpdateTime")
+#pragma comment (linker, "/export:_ohBoostVersion")
+#pragma comment (linker, "/export:_ohFilter")
+#pragma comment (linker, "/export:_ohFunctionCount")
+#pragma comment (linker, "/export:_ohPack")
+#pragma comment (linker, "/export:_ohRangeRetrieveError")
+#pragma comment (linker, "/export:_ohRemoveInvalidColumns")
+#pragma comment (linker, "/export:_ohRemoveInvalidRows")
+#pragma comment (linker, "/export:_ohRetrieveErrorImpl")
+#pragma comment (linker, "/export:_ohStringConcatenate")
+#pragma comment (linker, "/export:_ohStringSplit")
+#pragma comment (linker, "/export:_ohTrigger")
+#pragma comment (linker, "/export:_ohVbaError")
+#pragma comment (linker, "/export:_ohVersion")
+#pragma comment (linker, "/export:_ohRange")
+#pragma comment (linker, "/export:_ohObjectLoad")
+#pragma comment (linker, "/export:_ohObjectLoadString")
+#pragma comment (linker, "/export:_ohObjectSave")
+#pragma comment (linker, "/export:_ohObjectSaveString")
+#pragma comment (linker, "/export:_ohObjectPropertyNames")
+#pragma comment (linker, "/export:_ohObjectPropertyValues")
+
+
+#endif
+
