100d10dea7c283e697a49f73e2ec97b7b9d01cb8
Sergey Khorev <sergey.khorev@gmail.com>
2013-01-11 13:58:57 +0400

    Add quote wrapper

--- cpp/qlQuote.cpp
+++ cpp/qlQuote.cpp
@@ -4,9 +4,29 @@
 
 using namespace QuantLib;
 
+#ifdef QLTRACK_ALLOCATIONS
+// very minimal implementation to check that all objects are actually freed
+class QuoteWrapper: public Quote {
+  public:
+    QuoteWrapper(Quote *quote): quote_(quote) {TP2("wrapped", quote); TP2("wrapper", this);}
+    virtual ~QuoteWrapper() { TP2("destroying underlying", quote_); delete quote_; }
+    Real value() const {return quote_->value();}
+    bool isValid() const {return quote_->isValid();}
+  private:
+    Quote *quote_;
+};
+template <class T>
+Quote *wrap(T *q) {
+  return new QuoteWrapper(alloc(q));
+}
+#else
+template <class T>
+Quote *wrap(T *q) { return alloc(q); }
+#endif
+
 qlQuote *qlSimpleQuote(double value, char **e) {
   try {
-    return ret(new qlQuote(alloc(new SimpleQuote(value))));
+    return ret(new qlQuote(wrap(new SimpleQuote(value))));
   } catch (std::exception& er) {
     return handleException<qlQuote *>(e, er);
   }
