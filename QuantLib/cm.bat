bee8f11b11409780e59e8dd942f1ab2e332bdc60
Sergey Khorev <sergey.khorev@gmail.com>
2013-01-24 12:57:06 +0400

    Link libstdc++ and libgcc statically on windows

--- QuantLib/cm.bat
+++ QuantLib/cm.bat
@@ -1,3 +1,3 @@
 :: compile C++ files
 mkdir cpp
-%DEVLIBS%\MinGW\bin\mingw32-make.exe -f M EXTRA="-lstdc++ -I%DEVLIBS%\QuantLib-1.2.1 -I%DEVLIBS%\boost_1_50_0 -L%DEVLIBS%\QuantLib-1.2.1\ql\bin\15b474156460f41fe64f8ba9fcb070b1" %*
+%DEVLIBS%\MinGW\bin\mingw32-make.exe -f M EXTRA="-I%DEVLIBS%\QuantLib-1.2.1 -I%DEVLIBS%\boost_1_50_0 -L%DEVLIBS%\QuantLib-1.2.1\ql\bin\15b474156460f41fe64f8ba9fcb070b1" %*
--- QuantLib/cmt.bat
+++ QuantLib/cmt.bat
@@ -1,3 +1,3 @@
 :: compile C++ files
 mkdir cpp
-%DEVLIBS%\MinGW\bin\mingw32-make.exe -f M EXTRA="-lstdc++ -L%DEVLIBS%\QuantLib-1.2.1\ql\bin\15b474156460f41fe64f8ba9fcb070b1 -I%DEVLIBS%\QuantLib-1.2.1 -I%DEVLIBS%\boost_1_50_0 -DQLTRACK_ALLOCATIONS" %*
+%DEVLIBS%\MinGW\bin\mingw32-make.exe -f M EXTRA="-L%DEVLIBS%\QuantLib-1.2.1\ql\bin\15b474156460f41fe64f8ba9fcb070b1 -I%DEVLIBS%\QuantLib-1.2.1 -I%DEVLIBS%\boost_1_50_0 -DQLTRACK_ALLOCATIONS" %*
