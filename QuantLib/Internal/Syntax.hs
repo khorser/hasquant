4e594cb76854e1d0c83076dfc3161b9f1138faee
Sergey Khorev <sergey.khorev@gmail.com>
2013-01-26 14:26:37 +0400

    Parse return values in Syntax

--- QuantLib/Internal/Syntax.hs
+++ QuantLib/Internal/Syntax.hs
@@ -2,21 +2,27 @@
 module QuantLib.Internal.Syntax
 where
 
+import Control.Monad(liftM)
 import Language.Haskell.TH
 import QuantLib.Internal.Date
 import QuantLib.Internal.Utils
 
 data NestedArg = IntN | DayN | DoubleN | ForeignPtrN
+  deriving (Show, Eq)
 
 data TopArg = IntA | WordA | DayA | StringA | DoubleA
   | OptDayA | ForeignPtrA | OptForeignPtrA
   | List NestedArg | List2 NestedArg NestedArg
+  deriving (Show, Eq)
 
 isAtomicTop :: Name -> Bool
 isAtomicTop x = x `elem` [''Int, ''Word, ''Day, ''String, ''Double]
 
-data RetVal = IntR | DayR | StringR | DoubleR
-  | OptDayR | ForeignPtrR | IOR RetVal
+data AtomicRet = IntR | WordR | DayR | DoubleR | OptDayR | ForeignPtrR
+  deriving (Show, Eq)
+
+data RetVal = AtomicRV AtomicRet | IORV AtomicRet
+  deriving (Show, Eq)
 
 nameToTop :: Name -> TopArg
 nameToTop n | n == ''Int = IntA
@@ -30,18 +36,31 @@ topArgs :: Type -> Q TopArg
 topArgs (ConT n) | isAtomicTop n = return $ nameToTop n
 topArgs t = fail $ "Unsupported top-level arg type: " ++ show t
 
-nameToRetVal :: Name -> RetVal
-nameToRetVal n | n == ''Int = IntR
-nameToRetVal n | n == ''Day = DayR
-nameToRetVal n | n == ''String = StringR
-nameToRetVal n | n == ''Double = DoubleR
-nameToRetVal n = error $ "Return type not supported: " ++ show n
+nameToRetVal :: Name -> Q AtomicRet
+nameToRetVal n | n == ''Int = return IntR
+nameToRetVal n | n == ''Word = return WordR
+nameToRetVal n | n == ''Day = return DayR
+nameToRetVal n | n == ''Double = return DoubleR
+nameToRetVal n = do
+  r <- reify n
+  case r of
+    TyConI (TySynD _ _ (AppT (ConT p) _))
+      -> if p == ''ForeignPtr
+           then return ForeignPtrR
+           else fail $ "Unsupported synonym type: " ++ show r
+    _ -> fail $ "Unsupported return type: " ++ show r
 
-compToRetVal :: Name -> Type -> RetVal
---compToRetVal n1 _ | n1 == ''ForeignPtr = ForeignPtrR -- won't work until the type if reified!
---compToRetVal n1 n2 | n1 == ''Maybe && n2 == ''Day = OptDayR
-compToRetVal n1 t2 | n1 == ''IO = undefined
-compToRetVal n1 n2 = error $ "Compound return type not supported: " ++ show n1 ++ " " ++ show n2
+compArgToRetVal :: Type -> Q AtomicRet
+compArgToRetVal (AppT (ConT m) (ConT d)) | m == ''Maybe && d == ''Day =
+  return OptDayR
+compArgToRetVal (ConT n) = nameToRetVal n
+compArgToRetVal t = fail $ "Unsupported compound type arg: " ++ show t
+
+compToRetVal :: Type -> Q RetVal
+compToRetVal (AppT (ConT n1) t2) | n1 == ''IO = do
+  r <- compArgToRetVal t2
+  return $ IORV r
+compToRetVal t = liftM AtomicRV $ compArgToRetVal t
 
 -- use WriterT?
 args :: Type -> Q ([TopArg], RetVal)
@@ -49,27 +68,37 @@ args (AppT (AppT ArrowT t1) t2) = do
   top <- topArgs t1
   (rest, ret) <- args t2
   return (top : rest, ret)
-args (ConT n) = return ([], nameToRetVal n)
-args (AppT (ConT n1) t2) = return ([], compToRetVal n1 t2)
+args (ConT n) = do
+  r <- nameToRetVal n
+  return ([], AtomicRV r)
+args t@(AppT _ _) = do
+  r <- compToRetVal t 
+  return ([], r)
 args t = fail $ "Unsupported type: " ++ show t
 
-fft :: Name -> ExpQ
-fft n =
-  do TyConI (TySynD _ _ (AppT (ConT p) _)) <- reify n
-     return (LitE $ StringL (show $ p == ''ForeignPtr))
-
 ffe :: Name -> ExpQ
-ffe n =
-  do VarI _ ft _ _ <- reify n
-     let s = a ft
-     return (LitE $ StringL s)
+ffe n = do
+  VarI _ ft _ _ <- reify n
+  s <- args ft
+  return (LitE $ StringL (show s))
 
-a :: Type -> String
-a x = case x of
-          AppT (AppT ArrowT t1) t2 -> a t1 ++ " -> " ++ a t2
-          AppT ListT t1 -> "[" ++ a t1 ++ "]"
-          AppT (AppT (TupleT 2) t1) t2 -> "(" ++ a t1 ++ ", " ++ a t2 ++ ")"
-          AppT (AppT (AppT (TupleT 3) t1) t2) t3 -> "(" ++ a t1 ++ ", " ++ a t2 ++ ", " ++ a t3 ++ ")"
-          AppT t1 t2 -> "(" ++ a t1 ++ " " ++ a t2 ++ ")"
-          ConT t -> show t
-          _ -> fail $ "Unsupported argument type: " ++ show x
+-- fft :: Name -> ExpQ
+-- fft n =
+--   do TyConI (TySynD _ _ (AppT (ConT p) _)) <- reify n
+--      return (LitE $ StringL (show $ p == ''ForeignPtr))
+-- 
+-- ffe :: Name -> ExpQ
+-- ffe n =
+--   do VarI _ ft _ _ <- reify n
+--      let s = a ft
+--      return (LitE $ StringL s)
+-- 
+-- a :: Type -> String
+-- a x = case x of
+--           AppT (AppT ArrowT t1) t2 -> a t1 ++ " -> " ++ a t2
+--           AppT ListT t1 -> "[" ++ a t1 ++ "]"
+--           AppT (AppT (TupleT 2) t1) t2 -> "(" ++ a t1 ++ ", " ++ a t2 ++ ")"
+--           AppT (AppT (AppT (TupleT 3) t1) t2) t3 -> "(" ++ a t1 ++ ", " ++ a t2 ++ ", " ++ a t3 ++ ")"
+--           AppT t1 t2 -> "(" ++ a t1 ++ " " ++ a t2 ++ ")"
+--           ConT t -> show t
+--           _ -> fail $ "Unsupported argument type: " ++ show x
