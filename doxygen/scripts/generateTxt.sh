c9a5485a01c536f558906f913b0e6650f5c2b221
Sergey Khorev <sergey.khorev@gmail.com>
2013-02-02 17:27:13 +0400

    remove protected structs, add text extracts

---
+++ doxygen/scripts/generateTxt.sh
@@ -0,0 +1,4 @@
+for f in *.xml ; do xsltproc.exe ../_extr.xslt  $f > $f.txt ; done
+for f in namespace_quant_lib*.xml ; do xsltproc.exe _extr.xslt  $f > $f.txt ; done
+
+for f in *.xml.txt ; do n=`head -1 $f|tr '<:>' '[_]' | tr -d ' '` ; cp $f $n.txt ; done
