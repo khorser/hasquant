e605c3c0c743946af631211e7aec683bff4e0fa7
Sergey Khorev <sergey.khorev@gmail.com>
2013-02-11 20:57:32 +0400

    Use -isystem instead of -I on windows

--- src/QuantLib/cm64.bat
+++ src/QuantLib/cm64.bat
@@ -1,3 +1,3 @@
 :: compile C++ files
 mkdir cpp
-mingw32-make.exe -f M EXTRA="-I%DEVLIBS%\QuantLib-1.2.1 -I%DEVLIBS%\boost_1_50_0 -L%DEVLIBS%\QuantLib-1.2.1\ql\bin\8f9e26465fec6fb65803b3f74e8765a6" dll %*
+mingw32-make.exe -f M EXTRA="-isystem %DEVLIBS%\QuantLib-1.2.1 -isystem %DEVLIBS%\boost_1_50_0 -L%DEVLIBS%\QuantLib-1.2.1\ql\bin\8f9e26465fec6fb65803b3f74e8765a6" dll %*
--- src/QuantLib/cmtd.bat
+++ src/QuantLib/cmtd.bat
@@ -1,3 +1,3 @@
 :: compile C++ files
 mkdir cpp
-mingw32-make.exe -f M EXTRA="-L%DEVLIBS%\QuantLib-1.2.1-noopt\ql\bin\d750f55955fa5ad66fec54f46f00c080 -I%DEVLIBS%\QuantLib-1.2.1-noopt -I%DEVLIBS%\boost_1_50_0 -DQLTRACK_ALLOCATIONS -g -O0" dll %*
+mingw32-make.exe -f M EXTRA="-L%DEVLIBS%\QuantLib-1.2.1-noopt\ql\bin\d750f55955fa5ad66fec54f46f00c080 -isystem %DEVLIBS%\QuantLib-1.2.1-noopt -isystem %DEVLIBS%\boost_1_50_0 -DQLTRACK_ALLOCATIONS -g -O0" dll %*
