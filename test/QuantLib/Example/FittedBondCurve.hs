911b694b17ab541a83340af1999b19e476889d13
Sergey Khorev <sergey.khorev@gmail.com>
2013-02-19 08:18:16 +0400

    more fittedbondcurve code

--- test/QuantLib/Example/FittedBondCurve.hs
+++ test/QuantLib/Example/FittedBondCurve.hs
@@ -6,22 +6,25 @@ module QuantLib.Example.FittedBondCurve
 where
 
 import Control.Monad(forM_)
+import Data.Time.Calendar
 
+import QuantLib.CashFlow.Leg
+import QuantLib.Instrument.Bond
+import QuantLib.Math.Interpolation
+import QuantLib.Quote
 import QuantLib.Settings
+import QuantLib.TermStructure.Trait
+import qualified QuantLib.TermStructure.Yield as TS
+import QuantLib.Time.BusinessDayConvention
 import QuantLib.Time.Calendar
-import QuantLib.Time.Frequency
 import QuantLib.Time.Date
+import QuantLib.Time.DateGenerationRule
 import QuantLib.Time.DayCounter
+import QuantLib.Time.Frequency
 import QuantLib.Time.Period
 import QuantLib.Time.Schedule
-import QuantLib.TermStructure.Yield
-import QuantLib.TermStructure.Trait
-import QuantLib.Math.Interpolation
 import QuantLib.Time.Unit
-import QuantLib.Time.BusinessDayConvention
-import QuantLib.Time.DateGenerationRule
 import QuantLib.Types
-import QuantLib.Quote
 
 
 data Result = Result
@@ -49,28 +52,35 @@ run = do
       ModifiedFollowing ModifiedFollowing Backward False Nothing Nothing
 
     qq <- asQuote q
-    hA <- fixedRateBondHelper qq (fromIntegral bondSettleDays) 100.0 s [c] dc ModifiedFollowing 100.0 Nothing
-    hB <- fixedRateBondHelper qq (fromIntegral bondSettleDays) 100.0 s [c] dc ModifiedFollowing 100.0 Nothing >>= asRateHelper
+    hA <- TS.fixedRateBondHelper qq (fromIntegral bondSettleDays) 100.0 s [c] dc ModifiedFollowing 100.0 Nothing
+    hB <- TS.fixedRateBondHelper qq (fromIntegral bondSettleDays) 100.0 s [c] dc ModifiedFollowing 100.0 Nothing
+            >>= asRateHelper
     return (hA, hB)) $
       zip3 cleanQuotes lengths coupons
 
   let (instrA, instrB) = unzip helpers
 
-  ts0 <- piecewiseYieldCurve' curveSettleDays cal instrB dc [] 1e-12 Discount LogLinear
+  ts0 <- TS.piecewiseYieldCurve' curveSettleDays cal instrB dc [] 1e-12 Discount LogLinear
 
-  -- results do not quite match with thos of FittedBondCurve QuantLib example
-  -- GHC messing up with floating point?
-
-  fittings <- sequence [exponentialSplinesFitting True,
-                simplePolynomialFitting 3 True,
-                nelsonSiegelFitting,
-                cubicBSplinesFitting [-30.0, -20.0,  0.0,  5.0, 10.0, 15.0, 20.0,  25.0, 30.0, 40.0, 50.0] True,
-                svenssonFitting]
+  -- results do not quite match with FittedBondCurve QuantLib example
+  -- if QLC is built with optimization level different from QuantLib
+  fittings <- sequence [TS.exponentialSplinesFitting True,
+                TS.simplePolynomialFitting 3 True,
+                TS.nelsonSiegelFitting,
+                TS.cubicBSplinesFitting [-30.0, -20.0,  0.0,  5.0, 10.0, 15.0, 20.0,  25.0, 30.0, 40.0, 50.0] True,
+                TS.svenssonFitting]
 
   firstIter <- mapM
-      (\f -> fittedBondDiscountCurve curveSettleDays cal instrA dc f tolerance maxEvals)
+      (\f -> TS.fittedBondDiscountCurve curveSettleDays cal instrA dc f tolerance maxEvals)
       fittings
   forM_ firstIter printOutput
+  let ci = [(c, i) | c<-firstIter, i<-instrA]
+  forM_ ci $
+    \(fbts, h) -> do
+      cfs <- TS.bond h >>= cashflows >>= \l -> cashFlows l False (Just bondSettle)
+      let ds = map (\(_, d, _) -> d) $ filter (\(_, _, oc) -> not oc) cfs
+      ts <- asYieldTermStructure fbts
+      parRate ts (bondSettle:ds) dc
 
   return $ Result 5.6
   where
@@ -85,8 +95,21 @@ run = do
     tolerance = 1e-10
     maxEvals = 5000
 
+    printOutput :: FittedBondDiscountCurve -> IO ()
     printOutput ts = do
       putStr "Reference date: "
-      asYieldTermStructure ts >>= asTermStructure >>= referenceDate >>= print
+      yts <- asYieldTermStructure ts
+      asTermStructure yts >>= TS.referenceDate >>= print
       putStr "Number of iterations: "
-      numberOfIterations ts >>= print
+      TS.numberOfIterations ts >>= print
+
+    parRate :: YieldTermStructure -> [Day] -> DayCounter -> IO ()
+    parRate ts ds dc = do
+      dfs <- mapM (\(d1, d2) -> do
+              dt <- yearFraction dc d1 d2 Nothing Nothing
+              df <- TS.discount ts d2 False
+              return $ df * dt) $
+                zip (drop 1 ds) (init ds)
+      df1 <- TS.discount ts (head ds) False
+      df2 <- TS.discount ts (last ds) False
+      putStrLn $ "Par rate: " ++ (show $ (df1 - df2) / sum dfs)
