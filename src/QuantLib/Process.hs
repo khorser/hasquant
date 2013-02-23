8235901aced57b38e7c93a9150399f0eda4aea26
Sergey Khorev <sergey.khorev@gmail.com>
2013-02-23 11:08:50 +0400

    Add Process.hs

---
+++ src/QuantLib/Process.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+module QuantLib.Process
+  (
+  )
+where
+
+import QuantLib.Internal.Syntax
+import QuantLib.Internal.Types
+import QuantLib.Internal.Utils
+import QuantLib.Types
+
+
+-- vim: set ft=haskell ff=unix ts=8 sts=2 sw=2 et:
