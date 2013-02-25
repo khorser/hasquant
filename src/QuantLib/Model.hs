5aec38aaead59383d71edd1cc14e434754fe0507
Sergey Khorev <sergey.khorev@gmail.com>
2013-02-25 15:26:45 +0400

    Add forgotten Model.hs

---
+++ src/QuantLib/Model.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+module QuantLib.Model
+  (
+  )
+where
+
+import QuantLib.Internal.Date
+import QuantLib.Internal.Syntax
+import QuantLib.Internal.Types
+import QuantLib.Internal.Utils
+import QuantLib.Types
+
+
+-- vim: set ft=haskell ff=unix ts=8 sts=2 sw=2 et:
