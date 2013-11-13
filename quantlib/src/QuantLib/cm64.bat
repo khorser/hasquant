0e11da3dcf9500a1c656c418259688c6d2bad2f9
Sergey Khorev <sergey.khorev@gmail.com>
2013-11-13 13:43:14 +0400

    Silence mkdir errors

--- quantlib/src/QuantLib/cm64.bat
+++ quantlib/src/QuantLib/cm64.bat
@@ -1,3 +1,3 @@
 :: compile C++ files
-mkdir cpp
+mkdir cpp 2>nul
 mingw32-make.exe -f M EXTRA="-isystem %DEVLIBS%\QuantLib-1.3 -isystem %DEVLIBS%\boost_1_53_0 -L%DEVLIBS%\QuantLib-1.3\ql\bin\8f9e26465fec6fb65803b3f74e8765a6 %*" dll
--- quantlib/src/QuantLib/cmtd.bat
+++ quantlib/src/QuantLib/cmtd.bat
@@ -1,3 +1,3 @@
 :: compile C++ files
-mkdir cpp
+mkdir cpp 2>nul
 mingw32-make.exe -f M EXTRA="-L%DEVLIBS%\QuantLib-1.3-noopt\ql\bin\d750f55955fa5ad66fec54f46f00c080 -isystem %DEVLIBS%\QuantLib-1.3-noopt -isystem %DEVLIBS%\boost_1_53_0 -DQLTRACK_ALLOCATIONS -g -O0 %*" dll
