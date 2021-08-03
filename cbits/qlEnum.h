f485aba46d2ff456dbefb051594d627ee5f26de4
Sergey Khorev <sergey.khorev@gmail.com>
2021-08-03 08:22:52 +0300

    Migrate more date functions

--- cbits/qlEnum.h
+++ cbits/qlEnum.h
@@ -1,16 +1,44 @@
 //ql/time/weekday.hpp
-    enum Weekday { Sunday    = 1,
-                   Monday    = 2,
-                   Tuesday   = 3,
-                   Wednesday = 4,
-                   Thursday  = 5,
-                   Friday    = 6,
-                   Saturday  = 7,
-                   Sun = 1,
-                   Mon = 2,
-                   Tue = 3,
-                   Wed = 4,
-                   Thu = 5,
-                   Fri = 6,
-                   Sat = 7
-    };
+enum Weekday { Sunday    = 1,
+  Monday    = 2,
+  Tuesday   = 3,
+  Wednesday = 4,
+  Thursday  = 5,
+  Friday    = 6,
+  Saturday  = 7,
+  Sun = 1,
+  Mon = 2,
+  Tue = 3,
+  Wed = 4,
+  Thu = 5,
+  Fri = 6,
+  Sat = 7
+};
+
+//ql/time/date.hpp
+enum Month { January   = 1,
+  February  = 2,
+  March     = 3,
+  April     = 4,
+  May       = 5,
+  June      = 6,
+  July      = 7,
+  August    = 8,
+  September = 9,
+  October   = 10,
+  November  = 11,
+  December  = 12,
+  Jan = 1,
+  Feb = 2,
+  Mar = 3,
+  Apr = 4,
+  Jun = 6,
+  Jul = 7,
+  Aug = 8,
+  Sep = 9,
+  Oct = 10,
+  Nov = 11,
+  Dec = 12
+};
+
+/* vim: set ft=cpp ff=unix ts=8 sts=2 sw=2 et: */
