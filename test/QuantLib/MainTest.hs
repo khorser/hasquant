7b4d9168ba74a3d2ebcc88526f2d7e75bc669f98
Sergey Khorev <sergey.khorev@gmail.com>
2021-08-05 16:13:52 +0300

    Add tests for ECB and IMM dates

--- test/QuantLib/MainTest.hs
+++ test/QuantLib/MainTest.hs
@@ -103,6 +103,52 @@ main = do
         maxDate `shouldBe` fromGregorian 2199 12 31
       it "leap years" $ do
         [False, True, False] `shouldBe` map isLeap [fromGregorian 2100 10 10, fromGregorian 2012 1 1, fromGregorian 1981 5 5]
+      it "read ISO date" $ do
+        Settings.keepingSettings' $ (read "2006-01-15") `shouldBe` (15 `january` 2006)
+      it "known ECB dates" $ do
+        Settings.keepingSettings' $ do
+          knownDates <- knownECBDates
+          null knownDates `shouldBe` False
+          knownDates' <- nextECBDates (Just minDate)
+          knownDates `shouldBe` knownDates'
+          mapM_ (\(d, p) -> do
+            isECBDate d `shouldReturn` True
+            let d1 = addDays (-1) d
+            isECBDate d1 `shouldReturn` False
+            nextECBDate (Just d1) `shouldReturn` d
+            nextECBDate (Just p) `shouldReturn` d)
+            (zip knownDates (minDate:knownDates))
+          let h = head knownDates
+          removeECBDate h
+          isECBDate h `shouldReturn` False
+          addECBDate h
+          isECBDate h `shouldReturn` True
+      it "IMM dates (long running)" $ do
+        let immCodes = [
+                "F0", "G0", "H0", "J0", "K0", "M0", "N0", "Q0", "U0", "V0", "X0", "Z0",
+                "F1", "G1", "H1", "J1", "K1", "M1", "N1", "Q1", "U1", "V1", "X1", "Z1",
+                "F2", "G2", "H2", "J2", "K2", "M2", "N2", "Q2", "U2", "V2", "X2", "Z2",
+                "F3", "G3", "H3", "J3", "K3", "M3", "N3", "Q3", "U3", "V3", "X3", "Z3",
+                "F4", "G4", "H4", "J4", "K4", "M4", "N4", "Q4", "U4", "V4", "X4", "Z4",
+                "F5", "G5", "H5", "J5", "K5", "M5", "N5", "Q5", "U5", "V5", "X5", "Z5",
+                "F6", "G6", "H6", "J6", "K6", "M6", "N6", "Q6", "U6", "V6", "X6", "Z6",
+                "F7", "G7", "H7", "J7", "K7", "M7", "N7", "Q7", "U7", "V7", "X7", "Z7",
+                "F8", "G8", "H8", "J8", "K8", "M8", "N8", "Q8", "U8", "V8", "X8", "Z8",
+                "F9", "G9", "H9", "J9", "K9", "M9", "N9", "Q9", "U9", "V9", "X9", "Z9"]
+        Settings.keepingSettings' $ do
+          mapM_ (\d -> do
+            imm <- nextIMMDate d False
+            isIMMDate imm False `shouldReturn` True
+            n <- nextIMMDate d True
+            imm > d `shouldBe` True
+            imm <= n `shouldBe` True
+            code <- immCode imm
+            immDate code d `shouldReturn` imm
+            mapM_ (\i -> do
+              immd <- immDate i d
+              immd >= d `shouldBe` True)
+              $ take 40 immCodes)
+            [minDate .. (addGregorianMonthsClip (-121) maxDate)]
 
     describe "frequencies and periods" $ do
       it "frequency to period" $ do
