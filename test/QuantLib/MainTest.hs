6f3eae6a40477eaf0bac616033cf9958c4815a22
Sergey Khorev <sergey.khorev@gmail.com>
2021-08-12 08:15:09 +0300

    Migrate leg tests

--- test/QuantLib/MainTest.hs
+++ test/QuantLib/MainTest.hs
@@ -20,7 +20,8 @@ import QuantLib.Calendar as Calendar
 import QuantLib.Currency(currency, Ccy(..))
 import qualified QuantLib.Schedule as Schedule
 import QuantLib.Math
-import QuantLib.InterestRate
+import qualified QuantLib.InterestRate as IR
+import QuantLib.CashFlow
 
 instance Arbitrary Period.Frequency where
   arbitrary = elements $ OtherFrequency `delete` [minBound .. ]
@@ -197,9 +198,9 @@ main = do
 
       it "daily" $
         Settings.keepingSettings' $ do
-          let startDate = 17 `january` 2012
+          let startD = 17 `january` 2012
           cal <- calendar TARGET
-          (Schedule.schedule (Just startDate) (addDays 7 startDate) (1, Days) cal Following Following Backward False Nothing Nothing >>= Schedule.dates)
+          (Schedule.schedule (Just startD) (addDays 7 startD) (1, Days) cal Following Following Backward False Nothing Nothing >>= Schedule.dates)
             `shouldReturn` [17 `january` 2012, 18 `january` 2012, 19 `january` 2012, 20 `january` 2012, 23 `january` 2012, 24 `january` 2012]
       it "end date with EoM adjustment" $
         Settings.keepingSettings' $ do
@@ -960,64 +961,171 @@ main = do
         mapM_ (\(x, p, _x1, _x2, _x3, _x4, x5) -> testRounding Ceiling x p x5) testData
 
     describe "Interest rate" $ do
-      let cases :: [(Double, Compounding, Frequency, Double, Compounding, Frequency, Double, Int)]
-          cases = [ (0.0800, Compounded,        Quarterly,   1.00, Continuous,            Annual, 0.0792, 4),
-                    (0.1200, Continuous,           Annual,   1.00, Compounded,            Annual, 0.1275, 4),
-                    (0.0800, Compounded,        Quarterly,   1.00, Compounded,            Annual, 0.0824, 4),
-                    (0.0700, Compounded,        Quarterly,   1.00, Compounded,        Semiannual, 0.0706, 4),
-                    (0.0100, Compounded,           Annual,   1.00,     Simple,            Annual, 0.0100, 4),
-                    (0.0200,     Simple,           Annual,   1.00, Compounded,            Annual, 0.0200, 4),
-                    (0.0300, Compounded,       Semiannual,   0.50,     Simple,            Annual, 0.0300, 4),
-                    (0.0400,     Simple,           Annual,   0.50, Compounded,        Semiannual, 0.0400, 4),
-                    (0.0500, Compounded, EveryFourthMonth,  1.0/3,     Simple,            Annual, 0.0500, 4),
-                    (0.0600,     Simple,           Annual,  1.0/3, Compounded,  EveryFourthMonth, 0.0600, 4),
-                    (0.0500, Compounded,        Quarterly,   0.25,     Simple,            Annual, 0.0500, 4),
-                    (0.0600,     Simple,           Annual,   0.25, Compounded,         Quarterly, 0.0600, 4),
-                    (0.0700, Compounded,        Bimonthly,  1.0/6,     Simple,            Annual, 0.0700, 4),
-                    (0.0800,     Simple,           Annual,  1.0/6, Compounded,         Bimonthly, 0.0800, 4),
-                    (0.0900, Compounded,          Monthly, 1.0/12,     Simple,            Annual, 0.0900, 4),
-                    (0.1000,     Simple,           Annual, 1.0/12, Compounded,           Monthly, 0.1000, 4),
-                    (0.0300, SimpleThenCompounded,       Semiannual,   0.25,               Simple,            Annual, 0.0300, 4),
-                    (0.0300, SimpleThenCompounded,       Semiannual,   0.25,               Simple,        Semiannual, 0.0300, 4),
-                    (0.0300, SimpleThenCompounded,       Semiannual,   0.25,               Simple,         Quarterly, 0.0300, 4),
-                    (0.0300, SimpleThenCompounded,       Semiannual,   0.50,               Simple,            Annual, 0.0300, 4),
-                    (0.0300, SimpleThenCompounded,       Semiannual,   0.50,               Simple,        Semiannual, 0.0300, 4),
-                    (0.0300, SimpleThenCompounded,       Semiannual,   0.75,           Compounded,        Semiannual, 0.0300, 4),
-                    (0.0400,               Simple,       Semiannual,   0.25, SimpleThenCompounded,         Quarterly, 0.0400, 4),
-                    (0.0400,               Simple,       Semiannual,   0.25, SimpleThenCompounded,        Semiannual, 0.0400, 4),
-                    (0.0400,               Simple,       Semiannual,   0.25, SimpleThenCompounded,            Annual, 0.0400, 4),
-                    (0.0400,           Compounded,        Quarterly,   0.50, SimpleThenCompounded,         Quarterly, 0.0400, 4),
-                    (0.0400,               Simple,       Semiannual,   0.50, SimpleThenCompounded,        Semiannual, 0.0400, 4),
-                    (0.0400,               Simple,       Semiannual,   0.50, SimpleThenCompounded,            Annual, 0.0400, 4),
-                    (0.0400,           Compounded,        Quarterly,   0.75, SimpleThenCompounded,         Quarterly, 0.0400, 4),
-                    (0.0400,           Compounded,       Semiannual,   0.75, SimpleThenCompounded,        Semiannual, 0.0400, 4),
-                    (0.0400,               Simple,       Semiannual,   0.75, SimpleThenCompounded,            Annual, 0.0400, 4)]
+      let cases :: [(Double, IR.Compounding, Frequency, Double, IR.Compounding, Frequency, Double, Int)]
+          cases = [ (0.0800, IR.Compounded,        Quarterly,   1.00, IR.Continuous,            Annual, 0.0792, 4),
+                    (0.1200, IR.Continuous,           Annual,   1.00, IR.Compounded,            Annual, 0.1275, 4),
+                    (0.0800, IR.Compounded,        Quarterly,   1.00, IR.Compounded,            Annual, 0.0824, 4),
+                    (0.0700, IR.Compounded,        Quarterly,   1.00, IR.Compounded,        Semiannual, 0.0706, 4),
+                    (0.0100, IR.Compounded,           Annual,   1.00,     IR.Simple,            Annual, 0.0100, 4),
+                    (0.0200,     IR.Simple,           Annual,   1.00, IR.Compounded,            Annual, 0.0200, 4),
+                    (0.0300, IR.Compounded,       Semiannual,   0.50,     IR.Simple,            Annual, 0.0300, 4),
+                    (0.0400,     IR.Simple,           Annual,   0.50, IR.Compounded,        Semiannual, 0.0400, 4),
+                    (0.0500, IR.Compounded, EveryFourthMonth,  1.0/3,     IR.Simple,            Annual, 0.0500, 4),
+                    (0.0600,     IR.Simple,           Annual,  1.0/3, IR.Compounded,  EveryFourthMonth, 0.0600, 4),
+                    (0.0500, IR.Compounded,        Quarterly,   0.25,     IR.Simple,            Annual, 0.0500, 4),
+                    (0.0600,     IR.Simple,           Annual,   0.25, IR.Compounded,         Quarterly, 0.0600, 4),
+                    (0.0700, IR.Compounded,        Bimonthly,  1.0/6,     IR.Simple,            Annual, 0.0700, 4),
+                    (0.0800,     IR.Simple,           Annual,  1.0/6, IR.Compounded,         Bimonthly, 0.0800, 4),
+                    (0.0900, IR.Compounded,          Monthly, 1.0/12,     IR.Simple,            Annual, 0.0900, 4),
+                    (0.1000,     IR.Simple,           Annual, 1.0/12, IR.Compounded,           Monthly, 0.1000, 4),
+                    (0.0300, IR.SimpleThenCompounded,       Semiannual,   0.25,               IR.Simple,            Annual, 0.0300, 4),
+                    (0.0300, IR.SimpleThenCompounded,       Semiannual,   0.25,               IR.Simple,        Semiannual, 0.0300, 4),
+                    (0.0300, IR.SimpleThenCompounded,       Semiannual,   0.25,               IR.Simple,         Quarterly, 0.0300, 4),
+                    (0.0300, IR.SimpleThenCompounded,       Semiannual,   0.50,               IR.Simple,            Annual, 0.0300, 4),
+                    (0.0300, IR.SimpleThenCompounded,       Semiannual,   0.50,               IR.Simple,        Semiannual, 0.0300, 4),
+                    (0.0300, IR.SimpleThenCompounded,       Semiannual,   0.75,           IR.Compounded,        Semiannual, 0.0300, 4),
+                    (0.0400,               IR.Simple,       Semiannual,   0.25, IR.SimpleThenCompounded,         Quarterly, 0.0400, 4),
+                    (0.0400,               IR.Simple,       Semiannual,   0.25, IR.SimpleThenCompounded,        Semiannual, 0.0400, 4),
+                    (0.0400,               IR.Simple,       Semiannual,   0.25, IR.SimpleThenCompounded,            Annual, 0.0400, 4),
+                    (0.0400,           IR.Compounded,        Quarterly,   0.50, IR.SimpleThenCompounded,         Quarterly, 0.0400, 4),
+                    (0.0400,               IR.Simple,       Semiannual,   0.50, IR.SimpleThenCompounded,        Semiannual, 0.0400, 4),
+                    (0.0400,               IR.Simple,       Semiannual,   0.50, IR.SimpleThenCompounded,            Annual, 0.0400, 4),
+                    (0.0400,           IR.Compounded,        Quarterly,   0.75, IR.SimpleThenCompounded,         Quarterly, 0.0400, 4),
+                    (0.0400,           IR.Compounded,       Semiannual,   0.75, IR.SimpleThenCompounded,        Semiannual, 0.0400, 4),
+                    (0.0400,               IR.Simple,       Semiannual,   0.75, IR.SimpleThenCompounded,            Annual, 0.0400, 4)]
 
-      let testCase :: (Double, Compounding, Frequency, Double, Compounding, Frequency, Double, Int) -> IO ()
+      let testCase :: (Double, IR.Compounding, Frequency, Double, IR.Compounding, Frequency, Double, Int) -> IO ()
           testCase (r, comp, freq, t, comp2, freq2, expected, prec) = do
             d1 <- today
             dc <- Schedule.dayCounter Schedule.Actual360
-            ir <- interestRate r dc comp freq
+            ir <- IR.interestRate r dc comp freq
             let d2 = addDays (truncate $ 360 * t + 0.5) d1
-            compoundf <- compoundFactor' ir d1 d2 d1 d2
-            disc <- discountFactor' ir d1 d2 d1 d2
+            compoundf <- IR.compoundFactor' ir d1 d2 d1 d2
+            disc <- IR.discountFactor' ir d1 d2 d1 d2
             abs (disc - 1.0/compoundf) `shouldSatisfy` (<= 1.0e-15)
-            ir2 <- equivalentRate' ir dc comp freq d1 d2 d1 d2
-            abs (rate ir - rate ir2) `shouldSatisfy` (<= 1.0e-15)
+            ir2 <- IR.equivalentRate' ir dc comp freq d1 d2 d1 d2
+            abs (IR.rate ir - IR.rate ir2) `shouldSatisfy` (<= 1.0e-15)
 
-            ir3 <- equivalentRate' ir dc comp2 freq2 d1 d2 d1 d2
-            expectedIR <- interestRate expected dc comp2 freq2
+            ir3 <- IR.equivalentRate' ir dc comp2 freq2 d1 d2 d1 d2
+            expectedIR <- IR.interestRate expected dc comp2 freq2
 
             roundingPrecision <- rounding' prec Closest 5
-            let r3 = applyRounding roundingPrecision (rate ir3)
-            abs(r3 - rate expectedIR) `shouldSatisfy` (<= 1.0e-17)
+            let r3 = applyRounding roundingPrecision (IR.rate ir3)
+            abs(r3 - IR.rate expectedIR) `shouldSatisfy` (<= 1.0e-17)
 
-            ir3' <- equivalentRate' ir dc comp2 freq2 d1 d2 d1 d2
-            let r3' = applyRounding roundingPrecision (rate ir3')
+            ir3' <- IR.equivalentRate' ir dc comp2 freq2 d1 d2 d1 d2
+            let r3' = applyRounding roundingPrecision (IR.rate ir3')
             abs(r3' - expected) `shouldSatisfy` (<= 1.0e-17)
-
 
       it "bulk test for conversions" $ do
         Settings.keepingSettings' $ mapM_ testCase cases
+
+    describe "cash flow leg" $ do
+      let checkInclusion :: Leg -> Int -> [(Int, Bool)] -> IO ()
+          checkInclusion l n x = do
+            td <- Settings.evaluationDate
+            mapM_ (\(days, expected) -> do
+              cfs <- cashFlows l Nothing (Just $ addDays (fromIntegral days) td)
+              let (_, _, o) = cfs !! n
+              expected `shouldBe` not o) x
+
+          checkNPV :: Leg -> IR.InterestRate -> Bool -> Double -> IO ()
+          checkNPV l r includeRef expected = do
+            td <- Settings.evaluationDate
+            v <- npvFromYield' l r includeRef (Just td) (Just td)
+            abs(v - expected) `shouldSatisfy` (<= 1.0e-6)
+
+      it "misc variants of settings" $
+        Settings.keepingSettings' $ do
+          let cases12 l = do
+                checkInclusion l 0 [(0, False), (1, False)]
+                checkInclusion l 1 [(0, True), (1, False), (2, False)]
+                checkInclusion l 2 [(1, True), (2, False), (3, False)]
+
+              cases34 l = do
+                checkInclusion l 0 [(0, True), (1, False)]
+                checkInclusion l 1 [(0, True), (1, True), (2, False)]
+                checkInclusion l 2 [(1, True), (2, True), (3, False)]
+          td <- today
+          Settings.setEvaluationDate (Just td)
+          l <- leg (zip (repeat 1.0) [td .. addDays 2 td])
+
+          Settings.setIncludeReferenceDateEvents False
+          Settings.setIncludeTodaysCashFlows Nothing
+          cases12 l
+        
+          -- 2)
+          Settings.setIncludeReferenceDateEvents False
+          Settings.setIncludeTodaysCashFlows (Just False)
+          cases12 l
+          -- 3)
+          Settings.setIncludeReferenceDateEvents True
+          Settings.setIncludeTodaysCashFlows Nothing
+          cases34 l
+        
+          -- 4)
+          Settings.setIncludeReferenceDateEvents True
+          Settings.setIncludeTodaysCashFlows $ Just True
+          cases34 l
+        
+          -- 5)
+          Settings.setIncludeReferenceDateEvents True
+          Settings.setIncludeTodaysCashFlows $ Just False
+          checkInclusion l 0 [(0, False), (1, False)]
+          checkInclusion l 1 [(0, True), (1, True), (2, False)]
+          checkInclusion l 2 [(1, True), (2, True), (3, False)]
+
+          -- 5)
+          Settings.setIncludeReferenceDateEvents True
+          Settings.setIncludeTodaysCashFlows $ Just False
+          checkInclusion l 0 [(0, False), (1, False)]
+          checkInclusion l 1 [(0, True), (1, True), (2, False)]
+          checkInclusion l 2 [(1, True), (2, True), (3, False)]
+        
+          dc <- Schedule.dayCounter $ Schedule.Actual365Fixed Schedule.Actual365FixedStandard
+          noDisc <- IR.interestRate 0.0 dc IR.Continuous Annual
+        
+          Settings.setIncludeTodaysCashFlows Nothing
+          checkNPV l noDisc False 2.0
+          checkNPV l noDisc True 3.0
+        
+          Settings.setIncludeTodaysCashFlows $ Just False
+          checkNPV l noDisc False 2.0
+          checkNPV l noDisc True 2.0
+
+      it "fixed rate leg as of default settlement date" $ do
+        td <- Settings.evaluationDate
+        cal <- calendar TARGET
+        sch <- Schedule.schedule (Just $ addGregorianMonthsClip (-2) td) (addGregorianMonthsClip 4 td) (6, Months) cal Unadjusted Unadjusted Backward False Nothing Nothing
+        dc <- Schedule.dayCounter Schedule.Actual360
+        cpn <- IR.interestRate 0.03 dc IR.Simple Annual
+        l <- fixedRateLeg sch [100.0] [cpn] Following dc cal
+        accP <- accruedPeriod l False Nothing
+        accP `shouldSatisfy` (/= 0)
+        accD <- accruedDays l False Nothing
+        accD `shouldSatisfy` (/= 0)
+        accA <- accruedAmount l False Nothing
+        accA `shouldSatisfy` (/= 0)
+
+{- include once pricers are implemented
+-- dynamic cast of coupon in Black pricer
+test_AccessViolation :: IO ()
+test_AccessViolation = keepingSettings' $ do
+  setEvaluationDate (Just $ 7 `april` 2010)
+  cal <- target
+  dc <- actual365Fixed
+  q <- simpleQuote 0.04875825 >>= asQuote
+  ts <- flatForward (9 `april` 2010) q dc Continuous Annual
+  v <- simpleQuote 0.10 >>= asQuote
+  vol <- constantOptionletVolatility' 2 cal ModifiedFollowing v dc
+  let p = (3, Months)
+  index3m <- usdLibor p (Just ts)
+  pricer <- blackIborCouponPricer vol
+  sch <- schedule (Just $ 20 `september` 2013) (20 `december` 2013) p cal Following Following Backward False Nothing Nothing
+  cpns <- iborLeg sch index3m [100] dc Following [2] [] [0.000115] [] [] False False
+  setCouponPricer cpns pricer
+  void $ nextCashFlowAmount cpns True Nothing
+  assertBool True
+-}
 
 -- vim: set ff=unix ts=8 sts=2 sw=2 et:
