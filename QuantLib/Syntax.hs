7d5b1176ca756fcf368cdec51b10bfb63ae8b993
Sergey Khorev <sergey.khorev@gmail.com>
2022-11-14 18:41:52 +0400

    Make signature parsing more permissive

--- QuantLib/Syntax.hs
+++ QuantLib/Syntax.hs
@@ -16,7 +16,11 @@ free1st n = do
   let vd = reverse vars
   return $ LamE (map VarP (tail vd ++ [head vd])) (foldr (\v e -> AppE e (VarE v)) (VarE fn) vars)
   where
-    arity (AppT (AppT ArrowT _) x) = 1 + arity x
-    arity (ForallT _ _ a@(AppT _ _)) = arity a
-    arity (AppT (AppT _ _) _) = 0
-    arity _ = error "Unsupported function type"
+    arity as = arity' as
+      where
+        arity' (AppT (AppT ArrowT _) x) = 1 + arity' x
+        arity' (ForallT _ _ a@(AppT _ _)) = arity' a
+        arity' (AppT _ _) = 0
+        arity' x = error $ "Unsupported signature part: " ++ show x ++ ", full arguments: " ++ show as
+
+-- vim: set ft=haskell ff=unix ts=8 sts=2 sw=2 et:
