5578401e3e2ec8a77b138d09eb578c6b5cc82892
Sergey Khorev <sergey.khorev@gmail.com>
2022-11-15 22:31:21 +0400

    Remove extra parenthesis

--- test/QuantLib/Example/TARF.hs
+++ test/QuantLib/Example/TARF.hs
@@ -62,7 +62,7 @@ run = do
   gen <- pathGenerator PseudoRandom proc grid 0 (size grid - 1) False
   pps <- replicateM trials $ nextNPV gen ds ycILS
   let (ps, sFwds) = unzip pps
-  let ff = map (\x -> roundTo ((realToFrac x)/(realToFrac trials)) fxrateDigits) $ deepFold sFwds (+)
+  let ff = map (\x -> roundTo (realToFrac x/realToFrac trials) fxrateDigits) $ deepFold sFwds (+)
   return $ Result ((`roundTo` notionalDigits) $ sum ps/fromIntegral trials/spot) fwds ff
   where
     valDate = 15 `august` 2022
