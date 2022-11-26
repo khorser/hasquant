276462853afa44be964d2bc070d342cec9746d39
Sergey Khorev <sergey.khorev@gmail.com>
2022-11-26 20:06:40 +0400

    Revert back to Simple setup, use pkgconfig, make package.yaml more idiomatic

--- Setup.hs
+++ Setup.hs
@@ -1,23 +1,2 @@
-import Control.Exception(onException)
-import Data.Maybe(fromJust)
-import System.Process(readProcess)
-import Distribution.Simple(defaultMainWithHooks, simpleUserHooks, confHook)
-import Distribution.Simple.LocalBuildInfo(localPkgDescr)
-import Distribution.Types.BuildInfo(cxxOptions, ldOptions)
-import Distribution.Types.Library(libBuildInfo)
-import Distribution.Types.PackageDescription(library)
-
-main = defaultMainWithHooks simpleUserHooks {confHook = qlConfHook}
-
-qlConfHook (pd, bi) f = do
-  lbi <- confHook simpleUserHooks (pd, bi) f
-  cxx <- readProcess "quantlib-config" ["--cflags"] "" `onException` return ""
-  ld <- readProcess "quantlib-config" ["--libs"] "" `onException` return ""
-  let lpd = localPkgDescr lbi
-      lib = fromJust $ library lpd
-      libbi = libBuildInfo lib
-      libbi' = libbi {cxxOptions = cxxOptions libbi ++ words cxx,
-        ldOptions = ldOptions libbi ++ filter (/= "-lQuantLib") (words ld)}
-      lib' = lib {libBuildInfo = libbi'}
-      lpd' = lpd {library = Just lib'}
-  return $ lbi {localPkgDescr = lpd'}
+import Distribution.Simple
+main = defaultMain
